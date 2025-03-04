4faf06fbfd98a20509b54e6f0c90c1fbeb00cc9e
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-03-04 23:57:09 +0400

    zaur/feat:change same

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { lusitana } from '../ui/fonts'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import { fetchRevenue } from '../lib/data'
 
-const dashboardPage = () => {
+const DashboardPage =async  () => {
+    const revenue =await fetchRevenue()
   return (
-    <div>
-      dashboardPage
-    </div>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
   )
 }
 
-export default dashboardPage
+export default DashboardPage
